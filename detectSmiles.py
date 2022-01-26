2157babe9600cc6cae8ad85b477d33f17933a2d7
Martin Beyer <martin.beyer@skymatic.de>
2022-01-26 13:15:14 +0100

    modify smiles check

--- detectSmiles.py
+++ detectSmiles.py
@@ -11,9 +11,9 @@ import os
 print("pwd=" + os.getcwd())
 
 #frame = cv2.imread("G:\\OneDrive\\OneDrive - uibk.ac.at\\uibk\\5th\\VC\\PS\\smiledetectionbemame\\dataset\\SMILEsmileD\\SMILEs\\positives\\positives7\\3.jpg", cv2.COLOR_BGR2GRAY)
-#frame = cv2.imread("dataset\\SMILEsmileD\\SMILEs\\positives\\positives7\\15.jpg")
+frame = cv2.imread("dataset\\SMILEsmileD\\SMILEs\\positives\\positives7\\15.jpg")
 #frame = cv2.imread("dataset\\SMILEsmileD\\SMILEs\\negatives\\negatives7\\5.jpg")
-frame = cv2.imread("1.jpg")
+#frame = cv2.imread("1.jpg")
 
 frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 frame_gray = cv2.equalizeHist(frame_gray)
@@ -33,12 +33,13 @@ for (x,y,w,h) in faces:
     faceROI = frame_gray[y:y+h,x:x+w]
     #-- In each face, detect smile
     smiles = smileCascade.detectMultiScale(faceROI)
-    #print(smiles)
-    #print(smiles.shape)
-    for (x2,y2,w2,h2) in smiles:
-        smile_center = (x + x2 + w2//2, y + y2 + h2//2)
-        radius = int(round((w2 + h2)*0.25))
-        frame = cv2.circle(frame, smile_center, radius, (255, 0, 0 ), 4)
+    #print(len(smiles))
+    if len(smiles) > 0: #or smiles.any()
+        print(smiles.size)
+        for (x2,y2,w2,h2) in smiles:
+            smile_center = (x + x2 + w2//2, y + y2 + h2//2)
+            radius = int(round((w2 + h2)*0.25))
+            frame = cv2.circle(frame, smile_center, radius, (255, 0, 0 ), 4)
 
 
 #cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
