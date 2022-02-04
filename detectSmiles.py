bb7f73e5358a14548c4ff71e1253c2220708c37e
Martin Beyer <martin.beyer@skymatic.de>
2022-02-04 13:41:03 +0100

    Refactor

--- detectSmiles.py
+++ detectSmiles.py
@@ -1,73 +1,71 @@
-import re
 import cv2
 import os
 import glob
 import itertools
+#from sklearn import metrics
+#import sklearn
 cascPathface = os.path.dirname(cv2.__file__) + "\data\haarcascade_frontalface_alt2.xml" #Windows path
 cascPathsmile = os.path.dirname(cv2.__file__) + "\data\haarcascade_smile.xml" #Windows path
-
-# cascPathsmile = os.path.dirname(cv2.__file__) + "/data/haarcascade_smile.xml" #unix path
-# cascPathface = os.path.dirname(cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml" #unix path
-
 print(cascPathface)
+
 faceCascade = cv2.CascadeClassifier(cascPathface)
 smileCascade = cv2.CascadeClassifier(cascPathsmile)
 
-def getFilenames(exts):
-    #fnames = [glob.glob(ext) for ext in exts]
-    #fnames = list(itertools.chain.from_iterable(fnames))
-    #return fnames
-    fnames = []
-    for files in glob.glob('dataset\\SMILEsmileD\\SMILEs\\**\\*.jpg', recursive=True):
-        fnames.append(files)
+class SmileDetector:
+    def __init__(self):
+        self.TP, self.FP, self.TN, self.FN = 0, 0, 0, 0 
+        self.positivesImages = self.getFilenames("positives")
+        self.negativesImages = self.getFilenames("negatives")
     
-    return fnames
+    # cascPathsmile = os.path.dirname(cv2.__file__) + "/data/haarcascade_smile.xml" #unix path
+    # cascPathface = os.path.dirname(cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml" #unix path
+    # TP - img in positive folder was detected as smile
+    # FP - img in negative folder was detected as smile
+    # TN - img in negative folder was detected as not smile
+    # FN - img in positive folder was detected as not smile
+    
+    def getFilenames(self, type):
+        fnames = []
+        for files in glob.glob('dataset\\SMILEsmileD\\SMILEs\\' + type + '\\*.jpg', recursive=True):
+            fnames.append(files)
+        return fnames
+    
 
+    def detectSmiles(self, frame, type):
+        frame = cv2.imread(frame)
+    
+        smiles = smileCascade.detectMultiScale(frame)
+        if type == 'positive':
+            #True Positives
+            if len(smiles) > 0:
+                self.TP += 1
+            #False Negative
+            else:
+                self.FN += 1
+                
+        elif type == 'negative':
+            #False Positive
+            if len(smiles) > 0:
+                self.FP += 1
+            #True Negative
+            else:
+                self.TN += 1
 
-#do for one image only
-global amountOfSmiles #TODO make class
-amountOfSmiles = 0
-frames = []
-#frame = cv2.imread("G:\\OneDrive\\OneDrive - uibk.ac.at\\uibk\\5th\\VC\\PS\\smiledetectionbemame\\dataset\\SMILEsmileD\\SMILEs\\positives\\positives7\\3.jpg", cv2.COLOR_BGR2GRAY)
-#frame = cv2.imread("dataset\\SMILEsmileD\\SMILEs\\positives\\positives7\\15.jpg")
-happyFrame = cv2.imread("dataset\\SMILEsmileD\\SMILEs\\negatives\\negatives7\\7.jpg")
-sadFrame = cv2.imread("dataset\\SMILEsmileD\\SMILEs\\negatives\\negatives7\\9.jpg")
-#frames.append(happyFrame)
-#frames.append(sadFrame)
-exts = ["*.jpg","*/*.jpg"]
-frames = getFilenames(exts)
-#print("OSPRINT", os.listdir("."))
+    def classifyAllImages(self):
+        for frame in self.positivesImages:
+            self.detectSmiles(frame, 'positive')
+        for frame in self.negativesImages:
+            self.detectSmiles(frame, 'negative')
 
-#print(frames)
+    def printConfusionMatrix(self):
+        print("Amount of detected smiles: ", self.TP + self.FP) 
+        print("Amount of images: ", len(self.positivesImages) + len(self.negativesImages))
 
-
-def detectSmile(frame, myAmountOfSmiles):
-    frame = cv2.imread(frame)
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame_gray = cv2.equalizeHist(frame_gray)
-
-    #faces = faceCascade.detectMultiScale(frame_gray)
-    smiles = smileCascade.detectMultiScale(frame_gray)#print(faces)
-    if len(smiles) > 0: #or smiles.any()
-        #print("smileSize", smiles.size)
-        for (x2,y2,w2,h2) in smiles:
-            smile_center = ( x2 + w2//2, y2 + h2//2)
-            radius = int(round((w2 + h2)*0.25))
-            frame = cv2.circle(frame, smile_center, radius, (255, 0, 0 ), 4)
-            myAmountOfSmiles += 1
-    return frame, myAmountOfSmiles
-
-finishedImages = []
-for frame in frames:
-    img, amountOfSmiles = detectSmile(frame, amountOfSmiles)
-    #cv2.imshow('Capture - Smile detection', img)
-    finishedImages.append(img)
-
-
-#im_v = cv2.vconcat([finishedImages[0], finishedImages[1]])
-#cv2.imshow('Capture - Smile detection', im_v)
-
-print("Amount of smiles: ", amountOfSmiles)
-print("Amount of images: ", len(frames))
-
-k = cv2.waitKey(0)
+        print("True Positives: ", self.TP)
+        print("False Positives: ", self.FP)
+        print("True Negatives: ", self.TN)
+        print("False Positives: ", self.FP)
+    
+smileDetector = SmileDetector()
+smileDetector.classifyAllImages()
+smileDetector.printConfusionMatrix()
