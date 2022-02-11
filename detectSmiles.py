cb222da2f2245f27dd70d997846b5190dd917c1b
Martin Beyer <martin.beyer@skymatic.de>
2022-02-11 18:49:54 +0100

    Add sciKit ConvMat

--- detectSmiles.py
+++ detectSmiles.py
@@ -3,11 +3,16 @@ import os
 import glob
 import itertools
 #from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+
 #import sklearn
-#cascPathface = os.path.dirname(cv2.__file__) + "\data\haarcascade_frontalface_alt2.xml" #Windows path
-#cascPathsmile = os.path.dirname(cv2.__file__) + "\data\haarcascade_smile.xml" #Windows path
-cascPathsmile = os.path.dirname(cv2.__file__) + "/data/haarcascade_smile.xml" #unix path
-cascPathface = os.path.dirname(cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml" #unix path
+cascPathface = os.path.dirname(cv2.__file__) + "\data\haarcascade_frontalface_alt2.xml" #Windows path
+cascPathsmile = os.path.dirname(cv2.__file__) + "\data\haarcascade_smile.xml" #Windows path
+#cascPathsmile = os.path.dirname(cv2.__file__) + "/data/haarcascade_smile.xml" #unix path
+#cascPathface = os.path.dirname(cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml" #unix path
 print(cascPathface)
 
 faceCascade = cv2.CascadeClassifier(cascPathface)
@@ -18,6 +23,8 @@ class SmileDetector:
         self.TP, self.FP, self.TN, self.FN = 0, 0, 0, 0 
         self.positivesImages = self.getFilenames("positives")
         self.negativesImages = self.getFilenames("negatives")
+        self.y_true = []
+        self.y_pred = []
     
 
     # TP - img in positive folder was detected as smile
@@ -35,23 +42,34 @@ class SmileDetector:
 
     def detectSmiles(self, frame, type):
         frame = cv2.imread(frame)
+
     
         smiles = smileCascade.detectMultiScale(frame)
         if type == 'positive':
+            self.y_true.append(1)
             #True Positives
             if len(smiles) > 0:
                 self.TP += 1
+                self.y_pred.append(1)
+
             #False Negative
             else:
                 self.FN += 1
+                self.y_pred.append(0)
+
                 
         elif type == 'negative':
+            self.y_true.append(0)
             #False Positive
             if len(smiles) > 0:
                 self.FP += 1
+                self.y_pred.append(1)
             #True Negative
             else:
                 self.TN += 1
+                self.y_pred.append(0)
+
+        
 
     def classifyAllImages(self):
         for frame in self.positivesImages:
@@ -67,6 +85,11 @@ class SmileDetector:
         print("False Positives: ", self.FP)
         print("True Negatives: ", self.TN)
         print("False Negatives: ", self.FN)
+
+        #print(self.y_true)
+        disp = ConfusionMatrixDisplay.from_predictions(self.y_true, self.y_pred)
+        disp.plot()
+        plt.show()
     
 smileDetector = SmileDetector()
 smileDetector.classifyAllImages()
